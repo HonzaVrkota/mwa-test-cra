894abb8b247de573611f1bc1fc0fc19bc5a25966
Honza Vrkota <vrkota.jan@hotmail.com>
2021-12-06 16:28:23 +0100

    Přidán kod z hodiny

--- tailwind-cra-app/src/Card.js
+++ tailwind-cra-app/src/Card.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Card = ({ title, content }) => {
+const Card = ({ completed, content }) => {
+  useEffect(() => {
+    console.log("Start CARD.JS");
+  }, []);
   return (
     <div className="w-2/3 bg-white h-auto tracking-wide mb-14 border border-black-800 mx-1 rounded-lg relative">
       <div className="small-banner w-1 h-20 bg-blue-600 absolute rounded-tl-md"></div>
-      <h5 className="text-2xl font-semibold pl-6 pt-6 pr-6 pb-2">{title}</h5>
+      <h5 className="text-2xl font-semibold pl-6 pt-6 pr-6 pb-2">
+        {completed ? "Kompletní" : "Nekompletní"}
+      </h5>
       <p className="text-md font-regular p-6 pt-2 text-gray-500">{content}</p>
     </div>
   );
